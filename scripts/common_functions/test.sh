42be9005afde5e9b39a14d3ead89c7f7ec402038
zoltanmajor-seon <zoltan.major@seon.io>
2025-07-24 09:06:20 +0200

    fixes

--- scripts/common_functions/test.sh
+++
@@ -1,5 +0,0 @@
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source ${SCRIPT_DIR}/jira.sh
-
-get_summary "$(get_work_item 1234)"
-get_type "$(get_work_item 1234)"
